6d2b46d9f7021670dc18cb46a0907cd20522c57b
Joey Garber <joseph.z.garber@gmail.com>
2024-07-17 21:10:52 -0400

    Turned off linter

--- lua/custom/plugins/typescript-tools.lua
+++ lua/custom/plugins/typescript-tools.lua
@@ -2,13 +2,4 @@ return {
 	"pmizio/typescript-tools.nvim",
 	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 	opts = {},
-	-- config = function()
-	-- 	require("typescript-tools").setup()
-	-- 	vim.keymap.set(
-	-- 		"n",
-	-- 		"<leader>tsa",
-	-- 		"<cmd>TSToolsAddMissingImports<cr>",
-	-- 		{ desc = "[T]ype[Script] Tools [A]dd Missing Imports" }
-	-- 	)
-	-- end,
 }
