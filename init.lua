98d75f647c8c058b50569800aa0cdc1d688a06db
Joey Garber <joseph.z.garber@gmail.com>
2024-07-18 19:11:13 -0400

    Updated which-key config

--- init.lua
+++ init.lua
@@ -196,11 +196,24 @@ require("lazy").setup({
 	-- after the plugin has been loaded:
 	--  config = function() ... end
 
-	{ -- Useful plugin to show you pending keybinds.
-		"folke/which-key.nvim",
-		event = "VimEnter", -- Sets the loading event to 'VimEnter'
-		opts = {},
-	},
+{
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+},
 
 	-- NOTE: Plugins can specify dependencies.
 	--
