67909306cddfeb3f5dbadaf0c90de8fac10c3aeb
Joey Garber <joseph.z.garber@gmail.com>
2025-07-24 19:59:09 -0400

    Remove lint

--- lua/custom/plugins/roslyn.lua
+++ lua/custom/plugins/roslyn.lua
@@ -1,3 +1,21 @@
+vim.lsp.config("roslyn", {
+	on_attach = function()
+		print("Roslyn server attached")
+	end,
+	settings = {
+		["csharp|inlay_hints"] = {
+			csharp_enable_inlay_hints_for_implicit_object_creation = true,
+			csharp_enable_inlay_hints_for_implicit_variable_types = true,
+		},
+		["csharp|code_lens"] = {
+			dotnet_enable_references_code_lens = true,
+		},
+		["csharp|formatting"] = {
+			dotnet_organize_imports_on_format = true,
+		},
+	},
+})
+
 return {
 	"seblyng/roslyn.nvim",
 	ft = "cs",
